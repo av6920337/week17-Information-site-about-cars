2a02d2f6f22890b2e002a8baa0e5bed391b04781
av6920337 <av6920337@gmail.com>
2024-04-13 21:11:04 +0200

    The work is done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # week17-Information-site-about-cars
+
+Hello! In this repository, a code has been created that is responsible for displaying information about prices and transport on the website.
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,6 +12,7 @@ class Transport{
     }
 }
 
+
 class Car extends Transport{
     constructor(type,price,brand,doors){
         super(type,price,brand);
@@ -39,7 +40,7 @@ class Bike extends Transport{
         brand: 'Audi',
         doors: 4,
         price: 4300000,
-        image: '<https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2020_Audi_e-Tron_Sport_50_Quattro.jpg/1200px-2020_Audi_e-Tron_Sport_50_Quattro.jpg>'
+        // image: '<https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2020_Audi_e-Tron_Sport_50_Quattro.jpg/1200px-2020_Audi_e-Tron_Sport_50_Quattro.jpg>'
         },
         {
         id: 2,
@@ -47,7 +48,7 @@ class Bike extends Transport{
         brand: 'Mercedes-Benz',
         doors: 4,
         price: 2800000,
-        image: '<https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg/300px-2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg>'
+        // image: '<https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg/300px-2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg>'
         },
         {
         id: 3,
@@ -55,7 +56,7 @@ class Bike extends Transport{
         brand: 'Harley-Davidson',
         maxSpeed: 210,
         price: 1300000,
-        image: '<https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2022/2022-iron-883/2022-iron-883-016/2022-iron-883-016-motorcycle.jpg>'
+        // image: '<https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2022/2022-iron-883/2022-iron-883-016/2022-iron-883-016-motorcycle.jpg>'
         },
         {
         id: 4,
@@ -63,10 +64,9 @@ class Bike extends Transport{
         brand: 'Harley-Davidson',
         maxSpeed: 220,
         price: 1400000,
-        image: '<https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png>'
+        // image: '<https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png>'
         }
     ];
-
 // Создаем экземпляры классов Car и Bike
 const cars = data.filter(item => item.type === 'car').map(item => new Car(item.type, item.price, item.brand, item.doors));
 const bikes = data.filter(item => item.type === 'bike').map(item => new Bike(item.type, item.price, item.brand, item.maxSpeed));
@@ -87,4 +87,3 @@ document.getElementById('bike-button').addEventListener('click', function() {
     const bikeInfo = bikes.map(bike => `Type: ${bike.type}, Brand: ${bike.brand}, Max Speed: ${bike.maxSpeed}, Price: ${bike.price}`).join('<br>');
     displayInfo(bikeInfo);
 });
-
